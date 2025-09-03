1201d530ff92584683eabf0519cb38b53d0ce70e
MatthewTyler27 <mltyler@students.unwsp.edu>
2025-09-03 18:23:15 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,5 +1,5 @@
 def hello_world():
     # Created as a Function to repeat later in the code 
-    print('Hello World.')
-#Using the function to type message
+    print('Hello World')
+#Using the function to type the message
 hello_world()
